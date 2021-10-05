d1f34fbe0d7a012e265f43abe1e20052c3198db0
Anh Le <a.v.le@wustl.edu>
2021-10-05 16:21:06 -0500

    Started on Login/Signup page with react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import React from 'react'
 import "./App.css";
+import Login from './Login';
+import Register from './Register';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App">Hello from GoodWatch </div>;
+    return (
+        <Router>
+            <Switch>
+                <Route path="/login" exact component={Login}/>
+                <Route path="/register" exact component={Register}/>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
