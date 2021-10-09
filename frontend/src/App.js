d12fcd403aa1110e4989f99230adc3df428b3a4b
Kien Ta <talekien1710@gmail.com>
2021-10-08 21:38:58 -0500

    prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react";
 import "./App.css";
-import Login from './Login';
-import Register from './Register';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from "./Login";
+import Register from "./Register";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/login" exact component={Login}/>
-                <Route path="/register" exact component={Register}/>
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
