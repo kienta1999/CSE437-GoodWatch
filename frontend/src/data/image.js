5f0ae313d2b88b193d22769da95b2603f34e405a
priyankaB99 <priyanka.iyr@gmail.com>
2021-10-19 17:05:22 -0500

    fix attempt

--- frontend/src/data/image.js
+++ frontend/src/data/image.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import keys from "../keys.js";
 
 const getMoviePoster = async (movieid) => {
-   const url = `https://imdb-api.com/API/Images/${keys.otherKey}/${movieid}`;
+   const url = `https://imdb-api.com/API/Images/k_shf5m9p8/${movieid}`;
     const res = await axios.get(url);
     if (res.status === 200) {
       const data = res.data.items[0].image;
